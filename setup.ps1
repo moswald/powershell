8d17002100dc5ac88aa8bfd9b6b87d289809823c
Matt Oswald <m@oswald.io>
2017-11-11 09:16:22 -0600

    forgot to update setup.ps1 to work anywhere

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,6 @@
 ﻿Remove-Item ~/documents/WindowsPowerShell/profile.ps1 -ea SilentlyContinue 2>&1 | out-null
 
-Out-File ~/documents/WindowsPowerShell/profile.ps1 -input ". ~/.ps/profile.ps1"
+$profileFolder = Split-Path $MyInvocation.MyCommand.Source
+Out-File ~/documents/WindowsPowerShell/profile.ps1 -input ". $profileFolder/profile.ps1"
 
 . ./profile.ps1
