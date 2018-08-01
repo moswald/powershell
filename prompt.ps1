b924d75d3ece71ac8ef5f60a4bce471737c5bf23
Matt Oswald <matt.oswald@microsoft.com>
2018-08-01 07:00:43 -0500

    fixing missing $err check, better formatting options for prompt

--- prompt.ps1
+++ prompt.ps1
@@ -1,4 +1,8 @@
 ﻿function Get-BatteryLevelPromptString {
+    param(
+        [bool]$err
+    )
+
     $battery = Get-WmiObject -Class Win32_Battery
 
     if ($battery -ne $nul) {
@@ -21,6 +25,10 @@
 }
 
 function Get-DebugPromptString {
+    param(
+        [bool]$err
+    )
+
     if ($PSDebugContext) {
         $debugColor = ?? { $Theme["prompt.dbg"] } { 'Maroon' }
         "%<fg=$debugColor>[DBG]%<fg=>"
@@ -28,12 +36,20 @@ function Get-DebugPromptString {
 }
 
 function Get-GitPromptString {
+    param(
+        [bool]$err
+    )
+
     $realLASTEXITCODE = $LASTEXITCODE
     Write-VcsStatus 2> $nul
     $global:LASTEXITCODE = $realLASTEXITCODE
 }
 
 function Get-PathPromptString {
+    param(
+        [bool]$err
+    )
+
     # swap '/' for '\'
     $cwd = $pwd.Path.Replace('\', '/')
 
@@ -65,18 +81,22 @@ function Get-PathPromptString {
 
     if ($err) {
         $errColor = ?? { $Theme["prompt.error"] } { 'DarkRed' }
-        $replacementColor = ?? { $Theme["prompt." + $replacement + ".error"]} { $errColor }
-        "%<fg=$replacementColor>$replacement%<fg=$errColor>$cwd%<fg=>"
+        $replacementColorFormatter = ?? { $Theme["prompt." + $replacement + ".error"]} { "%<fg=$errColor>" }
+        "$replacementColorFormatter$replacement%<bg=>%<fg=$errColor>$cwd%<fg=>"
     }
     else {
         $successColor = ?? { $Theme["prompt.success"] } { 'Green' }
-        $replacementColor = ?? { $Theme["prompt." + $replacement + ".success"]} { $successColor }
-        "%<fg=$replacementColor>$replacement%<fg=$successColor>$cwd%<fg=>"
+        $replacementColorFormatter = ?? { $Theme["prompt." + $replacement + ".success"]} { "%<fg=$successColor>" }
+        "$replacementColorFormatter$replacement%<bg=>%<fg=$successColor>$cwd%<fg=>"
     }
 }
 
 # PUSHD stack level prefix
 function Get-StackLevelPromptString {
+    param(
+        [bool]$err
+    )
+
     $stackCount = (Get-Location -Stack).Count
 
     if ($stackCount -gt 0) {
@@ -92,6 +112,10 @@ function Get-StackLevelPromptString {
 }
 
 function Get-TerminatorPromptChar {
+    param(
+        [bool]$err
+    )
+
     git branch >$nul 2>$nul
     if ($?) {
         "±"
@@ -102,12 +126,15 @@ function Get-TerminatorPromptChar {
 }
 
 function prompt {
-    $batteryLevel = Get-BatteryLevelPromptString
-    $debugPrompt = Get-DebugPromptString
-    $stackLevel = Get-StackLevelPromptString
-    $pathPrompt = Get-PathPromptString
-    $gitPrompt = Get-GitPromptString
-    $terminatorChar = Get-TerminatorPromptChar
+    # error on previous command?
+    $err = !$?
+
+    $batteryLevel = Get-BatteryLevelPromptString($err)
+    $debugPrompt = Get-DebugPromptString($err)
+    $stackLevel = Get-StackLevelPromptString($err)
+    $pathPrompt = Get-PathPromptString($err)
+    $gitPrompt = Get-GitPromptString($err)
+    $terminatorChar = Get-TerminatorPromptChar($err)
 
     $prompt = $pathPrompt
 
