1ff38ff077d60b060aebc8bd0dca415411ad3326
Matt Oswald <maoswald@microsoft.com>
2025-03-28 12:59:47 -0500

    updates

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,11 @@
 ﻿Remove-Item ~/documents/WindowsPowerShell/profile.ps1 -ea SilentlyContinue 2>&1 | out-null
+Remove-Item ~/documents/PowerShell/profile.ps1 -ea SilentlyContinue 2>&1 | out-null
 
 $profileFolder = Split-Path $MyInvocation.MyCommand.Source
 Out-File ~/documents/WindowsPowerShell/profile.ps1 -input ". $profileFolder/profile.ps1"
+Out-File ~/documents/PowerShell/profile.ps1 -input ". $profileFolder/profile.ps1"
 
+install-module terminal-icons
 Install-Module -Name PowershellHumanizer
 
 . $profileFolder/profile.ps1
